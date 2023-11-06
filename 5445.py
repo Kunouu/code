b57fad7e416e5c23f1b36131a4033e5da6f47e83
Kunouu <149992307+Kunouu@users.noreply.github.com>
2023-11-06 11:07:05 +0600

    c_ver1.1

--- 5445.py
+++ 5445.py
@@ -1,9 +1,20 @@
 def average(numbers):
-    return sum(numbers) / len(numbers)
+    if len(numbers) > 0:
+        return sum(numbers) / len(numbers)
+    else:
+        return None
 
 numbers = [1, 2, 3, 4, 5]
 result = average(numbers)
+
+if result is not None:
+    print(f"Среднее арифметическое: {result}")
+else:
+    print("Список пуст")
+
+import statistics
+
+numbers = [1, 2, 3, 4, 5]
+result = statistics.mean(numbers)
+
 print(f"Среднее арифметическое: {result}")
-
-
-
